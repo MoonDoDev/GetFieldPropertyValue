7796e2b03dc7fdc2e7a38dc7599e9118ff2d460d
Hector Gomez <moon.do.dev@gmail.com>
2025-02-25 17:42:20 -0500

    adding benchmark and updating readme file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace GetFieldPropertyValue;
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace GetFieldPropertyValue;
 
 /// <summary>
 /// 
@@ -10,6 +13,9 @@ internal record RecordData
 	/// </summary>
 	public readonly string[] ClassFields = [ "Field_01", "Field_02", "Field_03", "Field_04", "Field_05" ];
 	public readonly string[] ClassProperties = [ "Property_01", "Property_02" ];
+
+	public readonly List<string> FieldList = [ "Field_01", "Field_02", "Field_03", "Field_04", "Field_05" ];
+	public readonly List<string> PropertyList = [ "Property_01", "Property_02" ];
 
 	/// <summary>
 	/// Class fields
@@ -32,7 +38,7 @@ internal record RecordData
 	/// <typeparam name="T"></typeparam>
 	/// <param name="fieldName"></param>
 	/// <returns></returns>
-	public T GetFieldValue<T>( string fieldName )
+	internal T GetFieldValue<T>( string fieldName )
 	{
 		return ( T ) typeof( RecordData ).GetField( fieldName )!.GetValue( this )!;
 	}
@@ -43,9 +49,119 @@ internal record RecordData
 	/// <typeparam name="T"></typeparam>
 	/// <param name="propertyName"></param>
 	/// <returns></returns>
-	public T GetPropertyValue<T>( string propertyName )
+	internal T GetPropertyValue<T>( string propertyName )
 	{
 		return ( T ) typeof( RecordData ).GetProperty( propertyName )!.GetValue( this )!;
+	}
+}
+
+/// <summary>
+/// |---------------------------------------------------------|
+/// | Method                 | Mean     | Error    | StdDev   |
+/// |----------------------- |---------:|---------:|---------:|
+/// | GetFieldsByFor         | 778.3 ns | 14.65 ns | 14.39 ns |
+/// | GetPropertiesByFor     | 259.2 ns |  5.30 ns |  7.26 ns |
+/// | GetFieldsByForeach     | 798.3 ns | 28.84 ns | 85.03 ns |
+/// | GetPropertiesByForeach | 278.5 ns | 12.58 ns | 36.69 ns |
+/// |---------------------------------------------------------|
+/// | * Legends *                                             |
+/// | Mean   : Arithmetic mean of all measurements            |
+/// | Error  : Half of 99.9% confidence interval              |
+/// | StdDev : Standard deviation of all measurements         |
+/// | 1 ns   : 1 Nanosecond(0.000000001 sec)                  |
+/// |---------------------------------------------------------|
+/// </summary>
+public class BenchmarkClass
+{
+	private readonly RecordData _record;
+
+	/// <summary>
+	/// 
+	/// </summary>
+	public BenchmarkClass()
+	{
+		_record = new()
+		{
+			Field_01 = "f-one",
+			Field_02 = "f-two",
+			Field_03 = "f-three",
+			Field_04 = "f-four",
+			Field_05 = "f-five",
+			Property_01 = "p-one",
+			Property_02 = "p-two"
+		};
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	[Benchmark]
+	public string GetFieldsByFor()
+	{
+		string result = string.Empty;
+
+		for ( int i = 0; i < _record.ClassFields.Length; i++ )
+		{
+			result = string.Concat( result, _record.ClassFields[ i ], " = ",
+				_record.GetFieldValue<string>( _record.ClassFields[ i ] ), ", " );
+		}
+
+		return result;
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	[Benchmark]
+	public string GetPropertiesByFor()
+	{
+		string result = string.Empty;
+
+		for ( int i = 0; i < _record.ClassProperties.Length; i++ )
+		{
+			result = string.Concat( result, _record.ClassProperties[ i ], " = ",
+				_record.GetPropertyValue<string>( _record.ClassProperties[ i ] ), ", " );
+		}
+
+		return result;
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	[Benchmark]
+	public string GetFieldsByForeach()
+	{
+		string result = string.Empty;
+
+		foreach ( var field in _record.FieldList )
+		{
+			result = string.Concat( result, field, " = ",
+				_record.GetFieldValue<string>( field ), ", " );
+		}
+
+		return result;
+	}
+
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	[Benchmark]
+	public string GetPropertiesByForeach()
+	{
+		string result = string.Empty;
+
+		foreach ( var property in _record.PropertyList )
+		{
+			result = string.Concat( result, property, " = ",
+				_record.GetPropertyValue<string>( property ), ", " );
+		}
+
+		return result;
 	}
 }
 
@@ -59,6 +175,10 @@ public static class Program
 	/// </summary>
 	public static void Main()
 	{
+		// Run Class Benchmark
+		BenchmarkRunner.Run<BenchmarkClass>();
+
+		// Run process with console output
 		var record = new RecordData
 		{
 			Field_01 = "f-one",
@@ -71,21 +191,35 @@ public static class Program
 		};
 
 		Console.Clear();
-		Console.WriteLine( "Class fields:" );
+		Console.WriteLine( "Class fields ( string array ):" );
 
 		for ( int i = 0; i < record.ClassFields.Length; i++ )
 		{
-			Console.WriteLine( $"Value for field_{( i + 1 )} => {
-				record.GetFieldValue<string>( record.ClassFields[ i ] )}" );
+			Console.WriteLine( $"Value for field_{( i + 1 )} => {record.GetFieldValue<string>( record.ClassFields[ i ] )}" );
 		}
 
 		Console.WriteLine();
-		Console.WriteLine( "Class properties:" );
+		Console.WriteLine( "Class properties ( string array ):" );
 
 		for ( int i = 0; i < record.ClassProperties.Length; i++ )
 		{
-			Console.WriteLine( $"Value for property_{( i + 1 )} => {
-				record.GetPropertyValue<string>( record.ClassProperties[ i ] )}" );
+			Console.WriteLine( $"Value for property_{( i + 1 )} => {record.GetPropertyValue<string>( record.ClassProperties[ i ] )}" );
+		}
+
+		Console.WriteLine();
+		Console.WriteLine( "Class fields ( string list ):" );
+
+		foreach ( var field in record.FieldList )
+		{
+			Console.WriteLine( $"Value for {field} => {record.GetFieldValue<string>( field )}" );
+		}
+
+		Console.WriteLine();
+		Console.WriteLine( "Class properties ( string list ):" );
+
+		foreach ( var property in record.PropertyList )
+		{
+			Console.WriteLine( $"Value for {property} => {record.GetPropertyValue<string>( property )}" );
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # GetFieldPropertyValue
-Proyecto demo en C#.NET 8, que muestra una manera de obtener los valores de los 'Fields' o 'Properties' de una clase/record.  
+
+Este es un proyecto demo construido en Visual Studio 2022, y en C# de .NET 8, con el propósito de mostrar una manera cíclica de obtener los valores de los campos o propiedades de una clase/record.
+
+Para obtener los valores de los campos/propiedades, es necesario suministrar sus nombres, los cuales estamos almacenando en un array de tipo string `string[]`, e igualmente en una lista de strings `List<string>`.
+Estamos utilizando dos tipos diferentes para almacenar los nombres de los campos/propiedades para darle un valor adicional a este demo, y es mostrar el **Benchmark** en el uso de una sentencia `for` con un arreglo, y una sentencia `foreach` con una colección `IEnumerable`.
+
+En el presente proyecto tenemos una clase llamada `RecordData`, la cual tiene varios campos y algunas propiedades, y para obtener sus nombres, hemos creado dos métodos, los cuales son: 
+
+```
+internal T GetFieldValue<T>( string fieldName ) 
+internal T GetPropertyValue<T>( string propertyName )
+```
+
+La primera, como su nombre lo supone, es para obtener los valores de los campos, y la segunda para obtener los valores de las propiedades de la clase.
+
+Adicionalmente encontraremos la clase llamada `BenchmarkClass`, con la cual estamos evaluando el rendimiento al momento de obtener los valores de los campos/propiedades de la clase, usando `for` y `foreach`. El resultado de dicha evaluación se muestra a continuación (saca sus propias conclusiones):
+
+```
+|---------------------------------------------------------|
+| Method                 | Mean     | Error    | StdDev   |
+|----------------------- |---------:|---------:|---------:|
+| GetFieldsByFor         | 778.3 ns | 14.65 ns | 14.39 ns |
+| GetPropertiesByFor     | 259.2 ns |  5.30 ns |  7.26 ns |
+| GetFieldsByForeach     | 798.3 ns | 28.84 ns | 85.03 ns |
+| GetPropertiesByForeach | 278.5 ns | 12.58 ns | 36.69 ns |
+|---------------------------------------------------------|
+| * Legends *                                             |
+| Mean   : Arithmetic mean of all measurements            |
+| Error  : Half of 99.9% confidence interval              |
+| StdDev : Standard deviation of all measurements         |
+| 1 ns   : 1 Nanosecond(0.000000001 sec)                  |
+|---------------------------------------------------------|
+
+```
+> [!IMPORTANT]
+> 
+> Las métricas aqui expuestas pueden variar, dependiendo de las características del equipo donde se ejecute el test.
+
+## Dependencias
+
+```
+"BenchmarkDotNet" Version="0.14.0"
+```
+
+---------
+
+[**YouTube**](https://www.youtube.com/@hectorgomez-backend-dev/featured) - 
+[**LinkedIn**](https://www.linkedin.com/in/hectorgomez-backend-dev/) - 
+[**GitHub**](https://github.com/MoonDoDev/Jubatus.WebApi.Extensions)
